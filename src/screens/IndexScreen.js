72d7bc8901bde3c950c71f6bbc298013ac5dcbd8
Isaac Yanez <isaac231467@gmail.com>
2022-12-20 10:43:26 -0600

    made some small UI fixes so the blog post look a bit better

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,25 +1,45 @@
 import React, { useContext } from "react";
 import { View, Text, StyleSheet, FlatList, Button } from "react-native";
 import { Context } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 const IndexScreen = () => {
   const { state, addBlogPost } = useContext(Context);
 
   return (
     <View>
-      <Text> Hello from Index screen</Text>
       <Button title="Add Post" onPress={addBlogPost} />
       <FlatList
         data={state}
         keyExtractor={(blogPost) => blogPost.title}
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>;
+          return (
+            <View style={style.row}>
+              <Text style={style.title}>{item.title}</Text>
+              <Feather style={style.iconStyle} name="trash" />
+            </View>
+          );
         }}
       />
     </View>
   );
 };
 
-const style = StyleSheet.create({});
+const style = StyleSheet.create({
+  row: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingVertical: 20,
+    borderTopWidth: 1,
+    borderColor: "gray",
+    paddingHorizontal: 10,
+  },
+  title: {
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 24,
+  },
+});
 
 export default IndexScreen;
